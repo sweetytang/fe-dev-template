89eff3ac8dea218303fd00611750fb9f0188edfe
zhexianren <zhexianren@pinduoduo.com>
2023-06-21 00:42:11 +0800

    feat: webpack-bundle-analyzer

--- build/webpack_prod.config.js
+++ build/webpack_prod.config.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const baseConfig = require("./webpack_base.config");
 const { PRO_TEXT } = require("./utils/env");
 
@@ -8,5 +9,6 @@ module.exports = smp.wrap(
   merge(baseConfig, {
     mode: PRO_TEXT,
     devtool: "hidden-cheap-module-source-map", // 输出 SourceMap 方便在浏览器里调试 TS 代码
+    plugins: [new BundleAnalyzerPlugin()],
   })
 );
