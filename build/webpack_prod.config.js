0d7361d85fcfe91a6a9ea9910761dde859e2889f
zhexianren <zhexianren@pinduoduo.com>
2023-06-21 00:34:11 +0800

    feat: speed-measure-webpack-plugin

--- build/webpack_prod.config.js
+++ build/webpack_prod.config.js
@@ -1,8 +1,12 @@
 const { merge } = require("webpack-merge");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const baseConfig = require("./webpack_base.config");
 const { PRO_TEXT } = require("./utils/env");
 
-module.exports = merge(baseConfig, {
-  mode: PRO_TEXT,
-  devtool: "hidden-cheap-module-source-map", // 输出 SourceMap 方便在浏览器里调试 TS 代码
-});
+const smp = new SpeedMeasurePlugin();
+module.exports = smp.wrap(
+  merge(baseConfig, {
+    mode: PRO_TEXT,
+    devtool: "hidden-cheap-module-source-map", // 输出 SourceMap 方便在浏览器里调试 TS 代码
+  })
+);
